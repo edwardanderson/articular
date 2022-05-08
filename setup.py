fb4168f09997c9ddd6cb41c5e16fb76632fd4d1b
edwardanderson <anderson.edw@gmail.com>
2022-05-08 10:25:33 +0200

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='articular',
-    version='0.1.1',
+    version='0.1.2',
     description='Create knowledge graphs from Markdown documents.',
     url='https://github.com/edwardanderson/articular',
     author='Edward Anderson',
